070f33afb9e8a06e88f18856bc263c2967331180
Akash Kumar <akash.01raj@gmail.com>
2023-05-17 14:37:33 +0530

    react-toastify and github.io work is done

--- src/Agecalculator.js
+++ src/Agecalculator.js
@@ -1,10 +1,15 @@
 import React, {useState} from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import birthdaySong from "./bSong.mp3";
 
 export default function Agecalculator(){
     const [birthday, setBirthday] = useState("");
     const [dateValue, setDateValue] = useState({});
     const [intervalId, setIntervalId] = useState(0);
     
+    let wanted = new Audio(birthdaySong)
     //function for calculating the age and also check the error bounderies
     const handleCalculate = ()=>{ 
         const birthArr = birthday.split("-").map(item => Number(item));     //getting the year,month,date from input date field
@@ -13,18 +18,26 @@ export default function Agecalculator(){
         //checking the error bounderies
         if(!birthArr[0] || birthArr[0]>currentDate.getFullYear()){
             console.log("Enter valid Birthday !");
+            toast.error('🦄Enter valid Birth date!');
             return;
         }
-        if(birthArr[0] == currentDate.getFullYear()){
+        if(birthArr[0] === currentDate.getFullYear()){
             if(birthArr[1] > (currentDate.getMonth()+1)){
                 console.log("Enter valid month !");
+                toast.error('Enter valid month !');
                 return;
-            }else if(birthArr[1] == (currentDate.getMonth()+1) && birthArr[2] > currentDate.getDate()){
+            }else if(birthArr[1] === (currentDate.getMonth()+1) && birthArr[2] > currentDate.getDate()){
                 console.log("Enter valid date !");
+                toast.error('Enter valid date !');
                 return;
             }
         }
-
+        // if month and date is same then show Happy Birthday msg
+        if(birthArr[1] === (currentDate.getMonth()+1) && birthArr[2] === currentDate.getDate()){
+            toast('Happy Birthday');
+            wanted.play();
+        }
+        
         const newIntervalId = setInterval(()=>{
             const currentDate = new Date();
             let temp = {
@@ -38,13 +51,15 @@ export default function Agecalculator(){
             setDateValue(temp);
         }, 1000);
         setIntervalId(newIntervalId);               //storing the reference of setInterval into useState for dynamic implementation
+        
+        return ()=> clearInterval(intervalId);
     }
 
     //function for cleaning the field and reset to its default values
     const handleReset = ()=>{
         clearInterval(intervalId);
         setDateValue({});
-        setBirthday("yyyy-MM-dd");
+        setBirthday("dd-mm-yyyy");
     }
 
     return (
@@ -71,7 +86,6 @@ export default function Agecalculator(){
             <button className='reset-btn' type='reset' onClick={handleReset}>Reset</button>
         </div>
     </div>
-        
     );
 }
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Agecalculator from './Agecalculator';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className="App">
       <Agecalculator/>
+      <ToastContainer/>
+      <footer>
+        <p>&copy; created by Akash Kumar</p>
+      </footer>
     </div>
   );
 }
